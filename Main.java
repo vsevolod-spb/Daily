a636ec8d76ee3acf11bd02fee7d3dd903d3bd637
vsevolod-spb <vsevolod-spb@ya.ru>
2025-04-21 00:58:57 +0800

    write file

--- Main.java
+++ Main.java
@@ -1,22 +1,33 @@
+import java.io.FileWriter;
+import java.io.IOException;
 import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter; // Import the DateTimeFormatter class
+import java.time.format.DateTimeFormatter;
 
 public class Main {
-    public static void main(String[] args) {
-        String now = currentDateTime();
-        System.out.println("Hello, World");
+    public static void main(String[] args) throws IOException {
 
-        for (String arg : args) {
-
-            String output = String.format("[%s] %s", now, arg);
-            System.out.println(output);
+        if (args.length == 0) {
+            System.out.println("no arguments");
+            return;
         }
+
+        String now = currentDateTime();
+
+        try (FileWriter writer = new FileWriter("output.txt", true)) {
+            for (String arg : args) {
+                String output = String.format("[%s] %s\n", now, arg);
+                writer.write(output);
+            }
+            writer.flush();
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+        System.out.println("Success");
     }
 
     public static String currentDateTime() {
-        LocalDateTime currentTime = LocalDateTime.now(); // Create a date object
+        LocalDateTime currentTime = LocalDateTime.now();
         DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
         return currentTime.format(timeFormatter);
-
     }
 }
