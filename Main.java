0ee659f233f4a9bcec19c9e01d2d658f405947c0
vsevolod-spb <vsevolod-spb@ya.ru>
2025-04-21 00:05:17 +0800

    add packages, format dateTime,

--- Main.java
+++ Main.java
@@ -1,8 +1,22 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter; // Import the DateTimeFormatter class
+
 public class Main {
     public static void main(String[] args) {
+        String now = currentDateTime();
         System.out.println("Hello, World");
-        for (int i = 0; i < args.length; i++) {
-            System.out.println(args[i].toString());
+
+        for (String arg : args) {
+
+            String output = String.format("[%s] %s", now, arg);
+            System.out.println(output);
         }
+    }
+
+    public static String currentDateTime() {
+        LocalDateTime currentTime = LocalDateTime.now(); // Create a date object
+        DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        return currentTime.format(timeFormatter);
+
     }
 }
