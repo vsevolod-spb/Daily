5e5fd11684982ca2eec5ed6179cd1ce763eeabb2
vsevolod-spb <vsevolod-spb@ya.ru>
2025-04-22 00:39:01 +0800

    Add FileReader

--- Main.java
+++ Main.java
@@ -1,13 +1,28 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-
         if (args.length == 0) {
             System.out.println("no arguments");
+            return;
+        }
+        if (Objects.equals(args[0], "-R")) {
+            int countLine = 5;
+            if (args.length == 2) {
+                countLine = Integer.parseInt(args[1]);
+            }
+            List<String> lines = readFile("output.txt", countLine);
+            for (String line : lines) {
+                System.out.println(line);
+            }
             return;
         }
 
@@ -29,5 +44,18 @@ public class Main {
         LocalDateTime currentTime = LocalDateTime.now();
         DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
         return currentTime.format(timeFormatter);
+    }
+
+    public static List<String> readFile(String filename, int stringNumber) {
+        List<String> lines = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
+            int count = 1;
+            for (String line; (line = br.readLine()) != null && count <= stringNumber; count++) {
+                lines.add(line);
+            }
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+        return lines;
     }
 }
